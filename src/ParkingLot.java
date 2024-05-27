9bcd738cf0a4030150f51d8ec424741364fd09e6
8BitBoar <106558110+8BitBoar@users.noreply.github.com>
2024-05-27 14:23:52 -0400

    Implemented constructor, ability to park cars

--- src/ParkingLot.java
+++ src/ParkingLot.java
@@ -1,15 +1,47 @@
+import customExceptions.NoSlotAvailableException;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class ParkingLot {
 
     String parkingLotId;
     List<List<Slot>> slots;
 
-    public ParkingLot (String parkingLotId, int nfloors, int noOfSlotsPerFlr) {}
+    public ParkingLot (String parkingLotId, int nfloors, int noOfSlotsPerFlr) {
+        this.parkingLotId = parkingLotId;
+        for (int i = 0; i < nfloors; i++) {
+            slots.add(new ArrayList<>());
+            List<Slot> floorSlots = slots.get(i);
+            floorSlots.add(new Slot("truck"));
+            floorSlots.add(new Slot("bike"));
+            floorSlots.add(new Slot("bike"));
 
-    public String parkVehicle(String type, String regNo, String color) {
-        return "";
+            for (int j = 3; j < noOfSlotsPerFlr ;j++) {
+                slots.get(i).add(new Slot("car"));
+            }
+
+        }
     }
+
+    public String parkVehicle(String type, String regNo, String color) throws NoSlotAvailableException {
+        Vehicle vehicle = new Vehicle(type, regNo, color);
+
+        for (int i = 0; i<slots.size() ;i++) {
+            for (int j = 0; i<slots.get(i).size(); j++) {
+                Slot slot = slots.get(i).get(j);
+                if (Objects.equals(slot.type, type) && slot.vehicle==null) {
+                    slot.vehicle = vehicle;
+                    slot.ticketId=generateTicketId(i+1,j+1);
+                    return slot.ticketId;
+                }
+            }
+        }
+
+        throw new NoSlotAvailableException("No slot available for given type");
+    }
+
 
     public void unPark(String ticketId) {
 
@@ -25,5 +57,12 @@ public class ParkingLot {
 
     public void displayOccupiedSlots(String type) {
 
+    }
+
+
+    // private methods
+
+    private String generateTicketId(int flr, int slNo) {
+        return parkingLotId + "_" + flr + "_" + slNo;
     }
 }
