36b44d269a43e9cb990efde44449dacb166f2966
8BitBoar <106558110+8BitBoar@users.noreply.github.com>
2024-05-27 13:55:07 -0400

    Added framework for methods for the ParkingLot

--- src/ParkingLot.java
+++ src/ParkingLot.java
@@ -6,4 +6,24 @@ public class ParkingLot {
     List<List<Slot>> slots;
 
     public ParkingLot (String parkingLotId, int nfloors, int noOfSlotsPerFlr) {}
+
+    public String parkVehicle(String type, String regNo, String color) {
+        return "";
+    }
+
+    public void unPark(String ticketId) {
+
+    }
+
+    public int getNoOfOpenSlots(String type) {
+        return 0;
+    }
+
+    public void displayOpenSlots(String type) {
+
+    }
+
+    public void displayOccupiedSlots(String type) {
+
+    }
 }
